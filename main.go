2d0d8c69edc64ac46110842edd0b8eec52b40eee
Abubakar Khalid <Abubakar.Khalid@teo-intl.com>
2022-02-24 14:21:39 +0500

    subjects added for students

--- main.go
+++ main.go
@@ -12,17 +12,21 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type person struct {
-	ID      string `json:"id"`
-	Name    string `json:"name"`
-	Address string `json:"address"`
-}
 type subject struct {
-	ID           string `json:"id"`
-	Subject_Name string `json:"Sub_Name"`
+	ID        string `json:"id"`
+	SubName   string `json:"Sname"`
+	Deparment string `json:"Dept"`
 }
 
-var subjects []subject
+type person struct {
+	ID      string    `json:"id"`
+	Name    string    `json:"name"`
+	Address string    `json:"address"`
+	Subject []subject `json:"subj"`
+}
+
+// var subjects []subject;
+
 var persons []person
 
 func getAllPerson(w http.ResponseWriter, r *http.Request) {
@@ -68,16 +72,51 @@ func createPerson(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(Person)
 
 }
+func getSubjectofStudent(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, item := range persons {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item.Subject)
+		}
+	}
+}
+func AddSubject(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	for _, items := range persons {
+		if items.ID == params["id"] {
+			items.Subject = append(items.Subject, subject{ID: strconv.Itoa(rand.Intn(10000000000)),
+				Deparment: "Physics",
+				SubName:   "Geo-Physics",
+			})
+		}
+	}
+	json.NewEncoder(w).Encode(persons)
+
+}
+
 func main() {
 	r := mux.NewRouter()
 	port := os.Getenv("PORT")
-	persons = append(persons, person{ID: "1", Name: "Abubakar", Address: "H.no 285"})
-	persons = append(persons, person{ID: "2", Name: "Saad", Address: "H.no 282"})
+
+	persons = append(persons, person{ID: strconv.Itoa(rand.Intn(10000000000)), Name: "Abubakar", Address: "H.no 285", Subject: []subject{
+		{ID: strconv.Itoa(rand.Intn(10000000000)), Deparment: "Physics", SubName: "Analytical Physics"},
+		{ID: strconv.Itoa(rand.Intn(10000000000)), Deparment: "Religion", SubName: "Islamiat"},
+		{ID: strconv.Itoa(rand.Intn(10000000000)), Deparment: "Software", SubName: "OOP"},
+		{ID: strconv.Itoa(rand.Intn(10000000000)), Deparment: "Chemistry", SubName: "Elements"},
+	}})
+
+	persons = append(persons, person{ID: strconv.Itoa(rand.Intn(10000000000)), Name: "Saad", Address: "H.no 281", Subject: []subject{
+		{ID: strconv.Itoa(rand.Intn(10000000000)), Deparment: "Physics", SubName: "Analytical Physics"},
+		{ID: strconv.Itoa(rand.Intn(10000000000)), Deparment: "Chemistry", SubName: "Elements"},
+	}})
 
 	r.HandleFunc("/getAllperson", getAllPerson).Methods("GET")
 	r.HandleFunc("/getpersonbyID/{id}", getPersonByid).Methods("GET")
 	r.HandleFunc("/createPerson", createPerson).Methods("Post")
 	r.HandleFunc("/byName", getPersonByName).Methods("GET")
+	r.HandleFunc("/seeSubjects/{id}", getSubjectofStudent).Methods("GET")
+	r.HandleFunc("/AddSubject/{id}", AddSubject).Methods("POST")
 
 	fmt.Print("Server Starting at port: " + port)
 	log.Fatal(http.ListenAndServe(":"+port, r))
